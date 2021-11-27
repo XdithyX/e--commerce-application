4ec48667f6e83b4a7c0eb19a1c50f753823b52a1
Adithya Ajith <4adhi007@gmail.com>
2021-11-27 22:15:39 +0530

    shopping cart-navbar and cards

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products =[
+    {
+      name:"oneplus nord",
+      category:"Mobile",
+      description:"nice phone",
+      image :"https://helios-i.mashable.com/imagery/reviews/01MsKXhnucbieHlbObEgLpy/hero-image.fill.size_1200x1200.v1629522312.jpg"
+    },
+    {
+      name:"vivo v5",
+      category:"Mobile",
+      description:"nice phone",
+      image :"https://helios-i.mashable.com/imagery/reviews/01MsKXhnucbieHlbObEgLpy/hero-image.fill.size_1200x1200.v1629522312.jpg"
+    },
+    {
+      name:"IPhone11",
+      category:"Mobile",
+      description:"nice phone",
+      image :"https://helios-i.mashable.com/imagery/reviews/01MsKXhnucbieHlbObEgLpy/hero-image.fill.size_1200x1200.v1629522312.jpg"
+    },
+    {
+      name:"samsun s8",
+      category:"Mobile",
+      description:"nice phone",
+      image :"https://helios-i.mashable.com/imagery/reviews/01MsKXhnucbieHlbObEgLpy/hero-image.fill.size_1200x1200.v1629522312.jpg"
+    }
+  ]
+  res.render('index', {products});
+});
+
+module.exports = router;
